27e2a2226db9b625bf86cef6c329f1a5de84a871
Sergey <skiner_1988@mail.ru>
2021-01-27 13:07:25 +0800

    Flash mod
    Добавление команд в класс spi flash

--- prog/librarys/flash_spi.cpp
+++ prog/librarys/flash_spi.cpp
@@ -7,6 +7,8 @@ bool flash :: Init(SPI_HandleTypeDef *hspi, uint32_t StartAddr,  pins_spi_t Chip
   flash :: Hold = Hold;
   flash :: hspi = hspi;
   StartAddres = StartAddr;
+  uint8_t TxBuff[4] = {0x05};
+  uint8_t RxBuff[4] = {0x05};
   
   uint8_t *pAddr = (uint8_t*)&StartAddres;
   for(int i = 0; i < 3; ++i)
@@ -18,6 +20,11 @@ bool flash :: Init(SPI_HandleTypeDef *hspi, uint32_t StartAddr,  pins_spi_t Chip
   HAL_GPIO_WritePin(WriteProtect.GPIO_Port, WriteProtect.GPIO_Pin, GPIO_PIN_SET);
   HAL_GPIO_WritePin(Hold.GPIO_Port, Hold.GPIO_Pin, GPIO_PIN_SET);
   HAL_GPIO_WritePin(ChipSelect.GPIO_Port, ChipSelect.GPIO_Pin, GPIO_PIN_SET);
+  
+  HAL_GPIO_WritePin(ChipSelect.GPIO_Port, ChipSelect.GPIO_Pin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(hspi, TxBuff, 1, 10); // 
+  HAL_SPI_Receive(hspi, RxBuff, 1, 10);
+  HAL_GPIO_WritePin(ChipSelect.GPIO_Port, ChipSelect.GPIO_Pin, GPIO_PIN_SET);
   return true;
 }
 
@@ -25,7 +32,6 @@ void flash :: Read(settings_t *data){
   HAL_StatusTypeDef StatusSPI;
   uint32_t sizeData = sizeof(*data);
   uint8_t *ptrData = (uint8_t*)data;
-  
   
   HAL_GPIO_WritePin(ChipSelect.GPIO_Port, ChipSelect.GPIO_Pin, GPIO_PIN_RESET);
   
@@ -43,13 +49,27 @@ void flash :: Write(settings_t data){
   HAL_StatusTypeDef StatusSPI;
   uint32_t sizeData = sizeof(data);
   uint8_t *ptrData = (uint8_t*)&data;
+  uint8_t wren[1] = {0x06};
+  uint8_t sr[1] = {0x05};
+  uint8_t rd[5] = {0};
   
   HAL_GPIO_WritePin(ChipSelect.GPIO_Port, ChipSelect.GPIO_Pin, GPIO_PIN_RESET);
+  StatusSPI = HAL_SPI_Transmit(hspi, sr, 1, 10); // 
+  StatusSPI = HAL_SPI_Receive(hspi, rd, 2, 10);
+  HAL_GPIO_WritePin(ChipSelect.GPIO_Port, ChipSelect.GPIO_Pin, GPIO_PIN_SET);
   
+  HAL_GPIO_WritePin(ChipSelect.GPIO_Port, ChipSelect.GPIO_Pin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(hspi, wren, 1, 10); //
+  HAL_GPIO_WritePin(ChipSelect.GPIO_Port, ChipSelect.GPIO_Pin, GPIO_PIN_SET);
+  
+  HAL_GPIO_WritePin(ChipSelect.GPIO_Port, ChipSelect.GPIO_Pin, GPIO_PIN_RESET);
+  StatusSPI = HAL_SPI_Transmit(hspi, sr, 1, 10); // 
+  StatusSPI = HAL_SPI_Receive(hspi, rd, 2, 10);
+  HAL_GPIO_WritePin(ChipSelect.GPIO_Port, ChipSelect.GPIO_Pin, GPIO_PIN_SET);
+  
+  HAL_GPIO_WritePin(ChipSelect.GPIO_Port, ChipSelect.GPIO_Pin, GPIO_PIN_RESET);
   StatusSPI = HAL_SPI_Transmit(hspi, cmdWrite, 4, 10); // cmd write + addres
- 
   StatusSPI = HAL_SPI_Transmit(hspi, ptrData, sizeData, 10);
-  //StatusSPI = StatusSPI;
   HAL_GPIO_WritePin(ChipSelect.GPIO_Port, ChipSelect.GPIO_Pin, GPIO_PIN_SET);
   
   lastStatusSPI = StatusSPI;
